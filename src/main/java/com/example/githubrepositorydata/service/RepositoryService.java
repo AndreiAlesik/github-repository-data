a60bc61eb88f3b1a5098fa474474bd74031f953a
AndrewAlesik <olesik.andrei@gmail.com>
2023-08-25 13:22:58 +0200

    fix: type of method

--- src/main/java/com/example/githubrepositorydata/service/RepositoryService.java
+++ src/main/java/com/example/githubrepositorydata/service/RepositoryService.java
@@ -3,5 +3,5 @@ package com.example.githubrepositorydata.service;
 import com.example.githubrepositorydata.dto.ResponseObject;
 
 public interface RepositoryService {
-    ResponseObject getUserRepositories(String githubOwner);
+    ResponseObject<?> getUserRepositories(String githubOwner);
 }
